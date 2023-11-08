6ed06bc8f8666ca5ce83ae08fefe8c2c92d477e1
AlfawakhryDev <abdelrahman.alfawakhry@outlook.com>
2023-11-09 01:32:13 +0200

    Added folder: D02 - Task 02

---
+++ D02 - Task 02/D02 - Task 02/D02 - Task 02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace D02
+{
+
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        Array.Reverse(words);
+
+        string reversedString = string.Join(" ", words);
+
+        Console.WriteLine("Output: " + reversedString);
+    }
+}
+}
+
