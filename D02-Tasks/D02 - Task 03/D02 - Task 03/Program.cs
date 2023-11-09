f370571bd7398388ae2cf33cfd7f2585a464dfd0
AlfawakhryDev <abdelrahman.alfawakhry@outlook.com>
2023-11-09 02:12:56 +0200

    Added folder : D02 - Task 03

---
+++ D02-Tasks/D02 - Task 03/D02 - Task 03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace D02___Task_03
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i <= 99999999; i++)
+            {
+                int num = i;
+                while (num > 0)
+                {
+                    if (num % 10 == 1)
+                    {
+                        count++;
+                    }
+                    num /= 10;
+                }
+            }
+
+            Console.WriteLine("Total number of '1's: " + count);
+        }
+    }
+
+}
